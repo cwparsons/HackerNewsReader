a1888b84a5321c7c902bf7f6494cf383e11e9858
Christopher Parsons <cparsons@habaneroconsulting.com>
2019-02-02 15:30:45 -0500

    Added opening pages, comments in browser

--- Components/StorySummary/StorySummary.js
+++ Components/StorySummary/StorySummary.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Linking, Text, TouchableHighlight, View } from 'react-native';
 import styles from './styles';
 
 export class StorySummary extends React.PureComponent {
+	constructor(props) {
+		super(props);
+
+		this.onItemCommentPress = this.onItemCommentPress.bind(this);
+		this.onItemUrlPress = this.onItemUrlPress.bind(this);
+	}
+
+	onItemCommentPress() {
+		Linking.openURL(`https://news.ycombinator.com/item?id=${this.props.item.id}`);
+	}
+
+	onItemUrlPress() {
+		Linking.openURL(this.props.item.url);
+	}
+
 	render() {
 		return (
 			<View>
 				<View style={styles.row}>
 					<View>
-						<View style={styles.rankContainer}>
-							<Text style={styles.rankText}>{this.props.index}</Text>
-						</View>
+						<TouchableHighlight onPress={this.onItemUrlPress}>
+							<View style={styles.rankContainer}>
+								<Text style={styles.rankText}>{this.props.index}</Text>
+							</View>
+						</TouchableHighlight>
 					</View>
 					<View style={styles.textContainer}>
 						<View>
-							<Text style={[styles.titleText]}>{this.props.item.title}</Text>
+							<TouchableHighlight onPress={this.onItemUrlPress}>
+								<Text style={[styles.titleText]}>{this.props.item.title}</Text>
+							</TouchableHighlight>
 						</View>
 						<View style={styles.metadataContainer}>
 							<View>
@@ -23,9 +42,11 @@ export class StorySummary extends React.PureComponent {
 								</Text>
 							</View>
 							<View>
-								<Text style={[styles.metadataText]}>
-									{this.props.item.descendants} {this.props.item.descendants === 1 ? 'comment' : 'comments'}
-								</Text>
+								<TouchableHighlight onPress={this.onItemCommentPress}>
+									<Text style={[styles.metadataText]}>
+										{this.props.item.descendants} {this.props.item.descendants === 1 ? 'comment' : 'comments'}
+									</Text>
+								</TouchableHighlight>
 							</View>
 							<View>
 								<Text style={[styles.metadataText]}>
