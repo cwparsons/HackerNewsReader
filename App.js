d517154dc8b0d10610987c5c735ddb406b5ce7f8
Christopher Parsons <cparsons@habaneroconsulting.com>
2019-02-03 14:30:49 -0500

    Removed Sentry. Updated adaptive icon for Android.

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Sentry from 'sentry-expo';
 
 import { AppContainer } from './src/Screens/AppContainer';
-
-Sentry.config('***REMOVED***').install();
 
 export default class App extends React.PureComponent {
 	render() {
